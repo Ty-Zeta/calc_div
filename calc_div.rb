770e412cd52bf611b1de36f8f014eced16d8668f
Ty-Zeta <tyofzeta@gmail.com>
2017-06-26 12:00:33 -0400

    updated division with joke if you divide by infinity

--- calc_div.rb
+++ calc_div.rb
@@ -1,5 +1,6 @@
-def div(a, b)
-  return a / b
-  b > 0
-end
-p div(10, 2)
+p "Enter first number to divide from"
+n1 = gets.chomp.to_f
+p "Enter second number to divide into"
+n2 = gets.chomp.to_f
+n3 = n1 / n2
+p "The answer to #{n1} / #{n2} = #{n3} and beyond"
