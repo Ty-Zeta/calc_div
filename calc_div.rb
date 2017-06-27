a2266fba541ffd2d270961d2378122d395687979
Ty-Zeta <tyofzeta@gmail.com>
2017-06-27 11:04:41 -0400

    updated div with funtion

--- calc_div.rb
+++ calc_div.rb
@@ -1,6 +1,12 @@
 p "Enter first number to divide from"
+def div(n1, n2)
+end
 n1 = gets.chomp.to_f
 p "Enter second number to divide into"
 n2 = gets.chomp.to_f
-n3 = n1 / n2
-p "The answer to #{n1} / #{n2} = #{n3} and beyond"
+  if n2 == 0
+    p "I'm sorry Dave, I can't let you do that"
+  else
+    puts "#{n1} / #{n2} = #{n1 / n2}"
+  end
+div(n1, n2)
